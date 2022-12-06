0d4d80cb0c91c8a95b8c6961da4a8865785bb4d9
green332211 <lobachtolyan123@gmail.com>
2022-12-06 17:02:35 +0300

    Test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from "next/head";
+import React from 'react';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+  return <>
+    <Head>
+      <title>MyTop - наш лучший топ</title>
+      <link rel="icon" href="/favicon.ico"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet"/>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
+
+export default MyApp;
